495ad59ffa92fd17654a96561309519146203ae9
Cory <coryitpro@gmail.com>
2021-09-14 13:09:35 -0400

    Fix styling issues with safari

--- src/components/learn/index.js
+++ src/components/learn/index.js
@@ -10,13 +10,13 @@ const Learn = () => (
           What are Crypto Athletes?
         </div>
         <div className="learn-wrapper-comments-content">
-          CryptoAthletes are 10,000 unique players balling on the Ethereum
+          CryptoAtletes are 10,000 unique players balling on the Ethereum
           Blockchain in style. With different chains, teams, hair, and more
           fashionable styles, there are more than 450,000,000 different possible
-          options for our ballers, but only 10,000 will be made. All players
-          will be randomly generated, this means no 2 players will be the same.
-          We have a unique ranking system, some ballers might be better than
-          others, but there will never be a bad baller!
+          options for our ballers, but only 10,000 are made. All players will be
+          randomly generated, this means no 2 players will be the same. We have
+          a unique ranking system, some ballers might be better than others, but
+          there will never be a bad baller!
         </div>
       </div>
       <div className="learn-wrapper-img">
