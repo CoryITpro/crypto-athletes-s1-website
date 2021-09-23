109b76943e35d458712c1b1dce93a04035b85234
Cory <coryitpro@gmail.com>
2021-09-22 20:40:45 -0400

    Update migration cod

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -2,18 +2,6 @@ require("dotenv").config()
 const CryptoAthletes = artifacts.require("CryptoAthletes")
 const baseURI = process.env.BASE_URI
 
-const generateList = () => {
-  let tempList = []
-  const maxElementCount = process.env.MAX_ELEMENT
-
-  for (let i = 0; i < maxElementCount; i++) {
-    tempList.push(i + 1)
-  }
-
-  return tempList
-}
-
 module.exports = function (deployer) {
-  const defaultCounts = generateList()
-  deployer.deploy(CryptoAthletes, baseURI, defaultCounts)
+  deployer.deploy(CryptoAthletes, baseURI)
 }
