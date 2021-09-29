99c11d6a297dd3a1a9387681276241fd59660ba9
Cory <coryitpro@gmail.com>
2021-09-29 18:00:04 -0400

    Remove unnecessary function

--- src/helpers/timer.js
+++ src/helpers/timer.js
@@ -1,10 +1,3 @@
-const convertTimeZone = (date, timezoneString) =>
-  new Date(
-    (typeof date === "string" ? new Date(date) : date).toLocalString("en-US", {
-      timeZone: timezoneString,
-    })
-  )
-
 export const calculateTimeLeft = () => {
   const presaleDate = new Date(Date.UTC(2021, 9, 5, 23, 0, 0))
   const difference = presaleDate - new Date()
