e0d646e6652e33774afb25c699675e256b8508d4
Cory <coryitpro@gmail.com>
2021-10-15 15:28:57 -0400

    Update opensea url

--- src/constants/index.js
+++ src/constants/index.js
@@ -75,6 +75,6 @@ export const FAQs = [
     question: "How can I Mint a CryptoAthlete?",
     answer:
       "We will have a presale of 500 pieces exclusive to members of our discord server . After our official launch you can mint them on our website. Once they are sold out you can purchase them on the secondary market on",
-    url: "https://opensea.io/collection/cryptoathletes",
+    url: "https://opensea.io/collection/cryptoathleteshoopers",
   },
 ]
